4f149d840f419e97a845aabbd96edc32fa803d6c
Alakh23 <neeva02singh@gmail.com>
2024-03-24 10:49:08 +0530

    final code

--- Controllers/DeleteNotes.js
+++ Controllers/DeleteNotes.js
@@ -1,16 +1,17 @@
 import users from "../Models/users.js";
 
 const DeleteNotes = async (req, res) => {
-  console.log("hi");
+  // console.log("hi");
   try {
     //get the notes id and user email
     const { id, email } = req.query;
     //get the user
-    console.log(req.query, req.body, req.params);
+    // console.log(req.query);
+    // console.log(req.query, req.body, req.params);
     const user = await users.findOne({ email });
 
     user.notes = user.notes.filter((curr, index) => {
-      console.log(curr._id, id); // yaha se ye check ho rha ki _id ek object hai aur id ek string hai
+      // console.log(curr._id, id); // yaha se ye check ho rha ki _id ek object hai aur id ek string hai
       return JSON.stringify(curr._id) !== JSON.stringify(id);
       //return ke baad kuchh bhi execute nhi hota
     });
--- Controllers/GetAllNotes.js
+++ Controllers/GetAllNotes.js
@@ -4,14 +4,15 @@ const GetAllNotes = async (req, res) => {
   try {
     // console.log("hi");
     //get the data from frontend
-    const { email } = req.body;
+    const { email } = req.query;
+    // console.log(req.query);
     // get the user whose email matches with the entered email
     const user = await users.findOne({ email });
-    // console.log(user, user.notes);
+    console.log(user);
     return res.status(200).json({
       message: "Notes available",
       success: true,
-      data: user.data,
+      data: user.notes,
     });
   } catch (err) {
     return res.status(200).json({ message: err.message, success: false });
